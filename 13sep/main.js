8823185ba27fd9d0d40dcf9ad5112cb582c5586a
scarletalfaro <scarletalfaro9@gmail.com>
2022-09-13 14:39:26 -0300

    13sep

---
+++ 13sep/main.js
@@ -0,0 +1,22 @@
+const texto = "mi primera constante"
+let name = "scarlet"
+let toys = "buzz"
+let dog = "peter"
+let cat = "angelina"
+
+//tipo de datos
+let flag =true //bollean ,dicotomica
+let number = 1 // number,numeros enteros o decimales
+let text = "el texto va entre comillas" //sting, texto
+let arr=[1,2,3,4,8] //arrays,cada elemento tiene un indece y parten desde 0
+let arr2=["texto" , [1,2,3],4, true] //arrays
+
+arr[2] //acceder al valor 4
+arr[2]= 5 //asignando un nuevo valor
+
+// variablename[index]->acceder al valor en un arreglo
+
+
+let nothing =null // null representa un valor "vacio"o "nulo"
+let nodef= undefined //suele no usarse,ya que javascrip lo hace por si mismo
+let width // sin un valor asignado,esta variable es "undefined"
